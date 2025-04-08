133496e35eddccc44d3c6ae760eda87bbb5e3b7e
Igor Martins <igor.martins@ibridge.com.br>
2025-04-07 23:47:53 -0300

    two sum solutions

--- arrays/extra_hash_map.py
+++ arrays/extra_hash_map.py
@@ -2,6 +2,9 @@
 
 # Retornar os índices dos dois números que somados dão o valor de target
 
+# Hash Map
+# time: O(N)
+# space: O(N)
 def twoSum(nums: list[int], target: int) -> list[int]:
   d = {}
   
@@ -12,7 +15,17 @@ def twoSum(nums: list[int], target: int) -> list[int]:
     else:
       d[nums[i]] = i
   return []
-    
+
+# Brute Force
+# time: O(N²)
+# space: O(1)
+def twoSum2(nums: list[int], target: int) -> list[int]:
+  for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      if i + j == target:
+        return [i, j]
+  return []
+
 print(twoSum([2,7,1,3], 9)) # [0, 1]
 print(twoSum([3,2,3,6], 6)) # [0, 2]
 print(twoSum([3,2,4,9], 6)) # [1, 2]
