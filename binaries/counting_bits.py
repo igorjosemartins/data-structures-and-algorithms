053c92828b33fabf842d4ee4908ea6fe6d3bfedc
Igor Martins <igor.martins@ibridge.com.br>
2025-04-13 18:52:42 -0300

    counting bits O(n) solution

--- binaries/counting_bits.py
+++ binaries/counting_bits.py
@@ -13,4 +13,14 @@ def countBits(n: int) -> list[int]:
     arr.append(count)
   return arr
 
+# O(n)
+def countBits2(n: int) -> list[int]:
+  # inicializa um array de 0's com o tamanho correto
+  arr = [0] * (n + 1)
+  for i in range(1, n + 1):
+    # utiliza (i >> 1) para reusar um resultado anterior e soma com o bit de paridade (i & 1)
+    arr[i] = arr[i >> 1] + (i & 1)
+  return arr
+
 print(countBits(5)) # [0, 1, 1, 2, 1, 2]
+print(countBits2(5)) # [0, 1, 1, 2, 1, 2]
