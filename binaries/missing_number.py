5219c93cde78e8e0ce1e2e773f8d9aac2aa6e55b
Igor Martins <igor.martins@ibridge.com.br>
2025-04-13 13:17:03 -0300

    explanation of the XOR solution

--- binaries/missing_number.py
+++ binaries/missing_number.py
@@ -1,4 +1,18 @@
 # 268. Missing Number
+
+# Encontrar o número faltante de um array de sequência 0 a N
+
+# Solução com XOR:
+  # a ^ a = 0
+  # a ^ 0 = a
+  # A ordem das operações XOR não importam
+  # Portanto se fizermos XOR com todos os items do array e com todos os items do range de 0 a N, teremos:
+    # 0 ^ 1 ^ 3 ^ 0 ^ 1 ^ 2 ^ 3
+    # Ou : 0 ^ 0 | ^ 1 ^ 1 | ^ 3 ^ 3 | ^ 2
+      # => 0 ^ 0 = 0
+      # => 1 ^ 1 = 0
+      # => 3 ^ 3 = 0
+    # Retornando apenas o número que falta
 def missingNumber(nums: list[int]) -> int:
   x = 0
   for num in nums:
@@ -7,3 +21,5 @@ def missingNumber(nums: list[int]) -> int:
       x ^= i
 
   return x
+
+print(missingNumber([0, 1, 3])) # 2
