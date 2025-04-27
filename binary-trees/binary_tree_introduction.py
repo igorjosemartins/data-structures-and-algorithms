6d248a40ee6c93b24a803ad0dc814428b24a25bf
Igor Martins <igorjosemartins01@gmail.com>
2025-04-26 22:51:21 -0300

    inorder traversal

--- binary-trees/binary_tree_introduction.py
+++ binary-trees/binary_tree_introduction.py
@@ -49,6 +49,17 @@ class BinaryTree:
       result.append(node.val)
       self._recursive_pre_order_traversal(node.left, result)
       self._recursive_pre_order_traversal(node.right, result)
+
+  def inorder_traversal(self):
+    result = []
+    self._recursive_inorder_traversal(self.root, result)
+    return result
+  
+  def _recursive_inorder_traversal(self, node, result):
+    if node:
+      self._recursive_inorder_traversal(node.left, result)
+      result.append(node.val)
+      self._recursive_inorder_traversal(node.right, result)
     
 tree = BinaryTree()
 values = [5, 3, 1, 10, 15, 7]
@@ -62,3 +73,4 @@ for val in values:
 # print(tree.search(1))  # True
 
 print(tree.pre_order_traversal()) # [5, 3, 1, 10, 7, 15]
+print(tree.inorder_traversal())   # [1, 3, 5, 7, 10, 15]
