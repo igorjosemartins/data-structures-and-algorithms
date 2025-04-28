327645a1c17fc536e929fec91bd494ce340fad36
Igor Martins <igorjosemartins01@gmail.com>
2025-04-27 23:19:58 -0300

    inorder traversal problem

---
+++ binary-trees/inorder_traversal.py
@@ -0,0 +1,17 @@
+def inorderTraversal(root):
+  result = []
+    
+  def inorder(node, result):
+    if node:
+      inorder(node.left, result)
+      result.append(node.val)
+      inorder(node.right, result)
+  
+  inorder(root, result)
+
+  return result
+
+def inorderTraversal2(root):
+  def inorder(node):
+    return inorder(node.left) + [node.val] + inorder(node.right) if node else []
+  return inorder(root)
